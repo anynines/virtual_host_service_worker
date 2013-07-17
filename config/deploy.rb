8bd27d9c26449a598a1f6dd04b07c32b38d6839a
Oliver Wolf <owolf@anynines.com>
2013-07-17 12:18:36 +0200

    fix the deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,6 @@ require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
 #require "whenever/capistrano"
 
-#### Use the asset-pipeline
-load 'deploy/assets'
-
 ##### Stages #####
 set :stages, %w(production)
 
@@ -13,7 +10,6 @@ set :stages, %w(production)
 set :application, "ssl_gateway"
 set :deploy_to,   "/var/www/#{application}"
 set :user, "deploy"
-set :use_sudo, true
 
 ##### Default variables #####
 set :keep_releases, 10
@@ -25,18 +21,17 @@ set :repository, "git@github.com:avarteqgmbh/virtual_host_service_worker.git"
 ##### Additional Settings #####
 #set :deploy_via, :remote_cache
 set :ssh_options, { :forward_agent => true }
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_dsa")]
 default_run_options[:pty] = true
 
 ##### Overwritten and changed default capistrano tasks #####
 namespace :deploy do
 
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && sudo DAEMON_ENV=#{env} bin/virtual_host_service_worker --pidfile #{pidfile} start"
+    run "cd #{current_path} && DAEMON_ENV=#{env} bin/virtual_host_service_worker --pidfile #{pidfile} start"
   end
 
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && sudo bin/virtual_host_service_worker --pidfile #{pidfile} stop"
+    run "cd #{current_path} && bin/virtual_host_service_worker --pidfile #{pidfile} stop"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
@@ -53,7 +48,7 @@ namespace :deploy do
 end
   
 ##### After and Before Tasks #####
-before "deploy:additional_symlink"
+before "deploy:restart", "deploy:additional_symlink"
 after "deploy:restart", "deploy:cleanup"
 
 #require 'config/boot'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,7 @@
-role :app, "109.234.107.6", :primary => true
-role :web, "109.234.107.6", :primary => true
+role :app, "5.22.148.133", :primary => true
+role :web, "5.22.148.133", :primary => true
 
 set :user, "deploy"
-set :use_sudo, true
 set :env, "production"
 set :pidfile, "tmp/virtual_host_service_worker.pid"
 set :branch, fetch(:branch, "master")
