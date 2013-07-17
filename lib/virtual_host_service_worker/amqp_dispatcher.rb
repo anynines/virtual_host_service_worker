8449786d6ecaee2b54e1261abc1f5b3799225a88
Oliver Wolf <owolf@anynines.com>
2013-07-17 14:09:27 +0200

    added some output to the logfile

--- lib/virtual_host_service_worker/amqp_dispatcher.rb
+++ lib/virtual_host_service_worker/amqp_dispatcher.rb
@@ -7,8 +7,10 @@ module VirtualHostServiceWorker
     #
     def self.dispatch(payload)
       if payload['ssl_certificate'] and payload['ssl_ca_certificate'] and payload['ssl_key']
+        DaemonKit.logger.info("added a new vhost")
         VirtualHostServiceWorker::NginxVHostWriter.setup_v_host(payload)
       elsif payload['action'] == 'delete'
+        DaemonKit.logger.info("deleted a vhost")
         VirtualHostServiceWorker::NginxVHostWriter.delete_v_host(payload['server_name'])
       end
     end
