8c7a3defcac6f0b3f7684a0a1fe1d5f3552de559
Oliver Wolf <owolf@anynines.com>
2013-09-04 15:51:14 +0200

    Bugfix for the honeybadger integration.

--- config/post-daemonize/configure_app.rb
+++
@@ -1,7 +0,0 @@
-require 'yaml'
-
-DAEMON_ENV = 'development' unless defined?( DAEMON_ENV )
-APP_CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'application.yml'))[DAEMON_ENV]
-
-VirtualHostServiceWorker::NginxVHostWriter.write_shared_webserver_config_files
-
--- config/pre-daemonize/configure_app.rb
+++ config/pre-daemonize/configure_app.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'honeybadger'
 
 DAEMON_ENV = 'development' unless defined?( DAEMON_ENV )
 APP_CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'application.yml'))[DAEMON_ENV]
