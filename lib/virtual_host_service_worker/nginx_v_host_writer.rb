a01784c8c357dd488e5422eec6e8790bb88d1c41
Sven Schmidt <sschmidt@avarteq.de>
2016-07-04 14:47:39 +0200

    adding support for websockets via port 4443

--- lib/virtual_host_service_worker/nginx_v_host_writer.rb
+++ lib/virtual_host_service_worker/nginx_v_host_writer.rb
@@ -2,7 +2,7 @@ require 'erubis'
 require 'fileutils'
 
 module VirtualHostServiceWorker
-  
+
   ##
   # This class provides methods to add virtual hosts to an nginx configuration.
   # Public interface:
@@ -10,7 +10,7 @@ module VirtualHostServiceWorker
   #   self.write_shared_webserver_config_files
   #
   class NginxVHostWriter < VHostWriter
-    
+
     ##
     # Adds a new virtual hosts configured with a ssl certificate to the nginx config.
     # The virtual host information is passed as hash to the method. The hash must contain the
@@ -25,20 +25,20 @@ module VirtualHostServiceWorker
     def self.setup_v_host(payload)
       payload['server_name'] = payload['server_name'].downcase
       payload['server_aliases'] = payload['server_aliases'].downcase if payload['server_aliases']
-      
+
       write_bundled_certificates(payload['server_name'],
                                  payload['ssl_ca_certificate'],
                                  payload['ssl_certificate'])
-      
+
       write_ssl_key(payload['server_name'], payload['ssl_key'])
-      
+
       write_webserver_config(payload['server_name'], payload['server_aliases'])
-      
+
       link_webserver_config(payload['server_name'])
-      
+
       reload_config
     end
-    
+
     ##
     # Deletes the ssl certificates and the nginx config from a virutal host.
     #
@@ -47,16 +47,16 @@ module VirtualHostServiceWorker
       v_host_link = File.join(APP_CONFIG['v_host_link_dir'].split('/'), "#{server_name}.conf") if APP_CONFIG['v_host_link_dir']
       key_file    = File.join(APP_CONFIG['cert_dir'].split('.'), server_name.gsub('*', 'wild'), "#{server_name}.key")
       pem_file    = File.join(APP_CONFIG['cert_dir'].split('/'), server_name.gsub('*', 'wild'), "#{server_name}.pem")
-      
+
       execute_command("rm -f #{v_host_file}")
       execute_command("rm -f #{key_file}")
       execute_command("rm -f #{pem_file}")
       execute_command("rm -rf #{File.dirname(pem_file)}")
       execute_command("rm -f #{v_host_link}") if APP_CONFIG['v_host_link_dir']
-      
+
       reload_config
     end
-    
+
     ##
     # Writes the common cofig files which are shared by all virtual hosts.
     # If the files already exists they would be overridden so that the values form
@@ -66,39 +66,39 @@ module VirtualHostServiceWorker
     def self.write_shared_webserver_config_files
       shared_template_file = File.join(File.dirname(__FILE__), '..', '..', 'templates', 'nginx_shared.conf.erb')
       shared_template = Erubis::Eruby.new(File.read(shared_template_file))
-      
+
       shared_config_file = File.join(APP_CONFIG['shared_config'].split('/'))
       File.open(shared_config_file, 'w') do |f|
         f.write(shared_template.result)
       end
-      
+
       upstream_template_file = File.join(File.dirname(__FILE__), '..', '..', 'templates', 'nginx_cf_routers.conf.erb')
       upstream_template = Erubis::Eruby.new(File.read(upstream_template_file))
-      
+
       upstream_config_file = File.join(APP_CONFIG['upstream_config'].split('/'))
       File.open(upstream_config_file, 'w') do |f|
         f.write(upstream_template.result({
           :routers => APP_CONFIG['routers']
         }))
       end
-      
+
       reload_config
     end
-    
+
     protected
-    
+
     ##
-    # Write the actual virtual host configuration pointing to the certificate fiels. 
+    # Write the actual virtual host configuration pointing to the certificate fiels.
     #
     def self.write_webserver_config(server_name, server_aliases)
-      
+
       template_file = File.join(File.dirname(__FILE__), '..', '..', 'templates', 'nginx_v_host.conf.erb')
       template = Erubis::Eruby.new(File.read(template_file))
-      
+
       server_aliases ||= ''
       server_aliases = server_aliases.gsub(',', ' ')
-      
-      
+
+
       v_host_file = File.join(APP_CONFIG['v_host_config_dir'].split('/'), "#{server_name.gsub('*', 'wild')}.conf")
       v_host_config = template.result({
         :server_name => server_name,
@@ -107,11 +107,11 @@ module VirtualHostServiceWorker
       })
 
       FileUtils.rm(v_host_file) if File.exist?(v_host_file)
-      
+
       File.open(v_host_file, 'w') do |f|
         f.write(v_host_config)
       end
-      
+
     end
 
     ##
@@ -126,29 +126,29 @@ module VirtualHostServiceWorker
       v_host_link = File.join(APP_CONFIG['v_host_link_dir'].split('/'), "#{server_name.gsub('*', 'wild')}.conf")
       execute_command("ln -s #{v_host_file} #{v_host_link}") unless File.exists?(v_host_link)
     end
-    
+
     ##
     # Wirte the ssl key file to directory specified by the application config (config/application.yml).
     #
     def self.write_ssl_key(server_name, key)
       key_file = File.join(APP_CONFIG['cert_dir'].split('/'), server_name.gsub('*', 'wild'), "#{server_name.gsub('*', 'wild')}.key")
 
-      FileUtils.rm(key_file) if File.exist?(key_file)      
-      FileUtils.mkdir_p(File.dirname(key_file))      
+      FileUtils.rm(key_file) if File.exist?(key_file)
+      FileUtils.mkdir_p(File.dirname(key_file))
 
       File.open(key_file, 'w') do |f|
         f.write(key)
       end
     end
-    
+
     ##
     # Bundles the ca certificate and the ssl certificate into one pem file and write it into the
     # directory specified by the application config (config/application.yml).
     #
     def self.write_bundled_certificates(server_name, ca_cert, cert)
       pem_file = File.join(APP_CONFIG['cert_dir'].split('/'), server_name.gsub('*', 'wild'), "#{server_name.gsub('*', 'wild')}.pem")
-      
-      FileUtils.rm(pem_file) if File.exist?(pem_file)      
+
+      FileUtils.rm(pem_file) if File.exist?(pem_file)
       FileUtils.mkdir_p(File.dirname(pem_file))
 
       File.open(pem_file, 'w') do |f|
@@ -157,21 +157,21 @@ module VirtualHostServiceWorker
         f.write(ca_cert.strip) if ca_cert
       end
     end
-    
+
     ##
     # Couses nginx to realod the created configuration.
     #
     def self.reload_config
       execute_command("#{APP_CONFIG['nginx_command']} -s reload") if config_valid?
     end
-    
+
     ##
     # Checks if the created nginx cofiguration is valid.
     #
     def self.config_valid?
-      command = "#{APP_CONFIG['nginx_command']} -t -c #{APP_CONFIG['webserver_config']}"  
+      command = "#{APP_CONFIG['nginx_command']} -t -c #{APP_CONFIG['webserver_config']}"
       execute_command(command, 'Invalid nginx configuration')
     end
-    
+
   end
 end
--- lib/virtual_host_service_worker/v_host_writer.rb
+++ lib/virtual_host_service_worker/v_host_writer.rb
@@ -1,11 +1,11 @@
 module VirtualHostServiceWorker
 
   class VHostWriter
-    
+
     def self.setup_v_host(payload)
       raise NotImplementedError.new
     end
-    
+
     ##
     # exectue system commands and raises an execption if this fails.
     # This method should be used to trigger an webserver reload.
@@ -13,11 +13,11 @@ module VirtualHostServiceWorker
       stdout = `#{command} 2>&1`
 
       if $?.exitstatus != expected_return
-        raise "Exception on executing command: #{command}\n Custom Message: #{custom_message}\n Command-Result:  #{stdout}"
+        raise "Exception on executing command: #{command}\n Custom Message: #{custom_message}\n Command-Result: #{stdout}"
       end
       return true
     end
-    
+
   end
-  
+
 end
