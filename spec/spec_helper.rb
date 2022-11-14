070e1a1c53fe87d738b85f453522bec5a45a40c3
Matthias Folz <mfolz@anynines.com>
2022-11-14 13:29:08 +0100

    fix relative path test setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,16 +8,18 @@ require File.dirname(__FILE__) + '/../config/environment'
 
 FileUtils.mkdir_p('tmp/cert_dir')
 FileUtils.mkdir_p('tmp/v_host_config_dir')
+FileUtils.mkdir_p('tmp/v_host_link_dir')
 
 APP_CONFIG = {
   "amqp_channel"      => "channel",
   "queue_id"          =>  123,
-  "cert_dir"          => 'tmp/cert_dir',
-  "v_host_config_dir" => 'tmp/v_host_config_dir',
-  "webserver_config"  => 'tmp/webserver_config',
-  "shared_config"     => 'tmp/shared_config',
-  "upstream_config"   => 'tmp/upstream_config',
-  "nginx_command"     => 'spec/support/nginx_dummy',
+  "cert_dir"          => File.expand_path('../../tmp/cert_dir', __FILE__) + '/',
+  "v_host_config_dir" => File.expand_path('../../tmp/v_host_config_dir', __FILE__) + '/',
+  "v_host_link_dir"   => File.expand_path('../../tmp/v_host_link_dir', __FILE__) + '/',
+  "webserver_config"  => File.expand_path('../../tmp/webserver_config', __FILE__),
+  "shared_config"     => File.expand_path('../../tmp/shared_config',__FILE__),
+  "upstream_config"   => File.expand_path('../../tmp/upstream_config', __FILE__),
+  "nginx_command"     => File.expand_path('../../spec/support/nginx_dummy', __FILE__),
   "routers"           => [],
 }
 
