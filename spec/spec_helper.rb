9d7cbc8c1f6b8f229122b1a909eaa88facaee8b5
Matthias Folz <mfolz@anynines.com>
2022-11-14 12:22:11 +0100

    add test config in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,27 @@ DAEMON_ENV = 'test' unless defined?( DAEMON_ENV )
 
 require 'rspec'
 require 'mocha/setup'
+require 'fileutils'
 
 require File.dirname(__FILE__) + '/../config/environment'
+
+FileUtils.mkdir_p('tmp/cert_dir')
+FileUtils.mkdir_p('tmp/v_host_config_dir')
+
+APP_CONFIG = {
+  "amqp_channel"      => "channel",
+  "queue_id"          =>  123,
+  "cert_dir"          => 'tmp/cert_dir',
+  "v_host_config_dir" => 'tmp/v_host_config_dir',
+  "webserver_config"  => 'tmp/webserver_config',
+  "shared_config"     => 'tmp/shared_config',
+  "upstream_config"   => 'tmp/upstream_config',
+  "nginx_command"     => 'spec/support/nginx_dummy',
+  "routers"           => [],
+}
+
 DaemonKit::Application.running!
 
 RSpec.configure do |config|
-
-  config.color_enabled = true
-  config.formatter = :documentation
   config.mock_with :mocha
-
 end
