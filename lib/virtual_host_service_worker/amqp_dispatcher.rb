761cba8c39c0bb3fb4a204675a9fb18816a8e7d7
Samuel <skretzschmann@anynines.com>
2023-09-05 16:46:44 +0200

    add logging for vhost create delete events

--- lib/virtual_host_service_worker/amqp_dispatcher.rb
+++ lib/virtual_host_service_worker/amqp_dispatcher.rb
@@ -1,32 +1,38 @@
 module VirtualHostServiceWorker
-  
+
   class AmqpDispatcher
-    
+
     ##
     # dispatches amqp-messages to method calls
     #
     def self.dispatch(payload)
       DaemonKit.logger.info("AMQP message received")
-      if payload['ssl_certificate'] and payload['ssl_ca_certificate'] and payload['ssl_key']
-        DaemonKit.logger.info("added a new vhost")
-        
-        if APP_CONFIG['use_haproxy'] == true
-          VirtualHostServiceWorker::HaproxyVHostWriter.setup_v_host(payload)
-        else
-          VirtualHostServiceWorker::NginxVHostWriter.setup_v_host(payload)
+      begin
+        if payload['ssl_certificate'] and payload['ssl_ca_certificate'] and payload['ssl_key']
+          DaemonKit.logger.info("adding a new vhost: #{payload['server_name']}")
+
+          if APP_CONFIG['use_haproxy'] == true
+            VirtualHostServiceWorker::HaproxyVHostWriter.setup_v_host(payload)
+            DaemonKit.logger.info("Succesfully added vhost #{payload['server_name']}")
+          else
+            VirtualHostServiceWorker::NginxVHostWriter.setup_v_host(payload)
+            DaemonKit.logger.info("Succesfully added vhost #{payload['server_name']}")
+          end
+
+        elsif payload['action'] == 'delete'
+          DaemonKit.logger.info("deleting vhost: #{payload['server_name']}")
+
+          if APP_CONFIG['use_haproxy'] == true
+            VirtualHostServiceWorker::HaproxyVHostWriter.delete_v_host(payload['server_name'])
+            DaemonKit.logger.info("Succesfully deleted vhost: #{payload['server_name']}")
+          else
+            VirtualHostServiceWorker::NginxVHostWriter.delete_v_host(payload['server_name'])
+            DaemonKit.logger.info("Succesfully deleted vhost: #{payload['server_name']}")
+          end
         end
-
-      elsif payload['action'] == 'delete'
-        DaemonKit.logger.info("deleted a vhost")
-
-        if APP_CONFIG['use_haproxy'] == true
-          VirtualHostServiceWorker::HaproxyVHostWriter.delete_v_host(payload['server_name'])
-        else
-          VirtualHostServiceWorker::NginxVHostWriter.delete_v_host(payload['server_name'])
-        end
-
+      rescue => e
+        DaemonKit.logger.error(e)
       end
     end
   end
-  
 end
